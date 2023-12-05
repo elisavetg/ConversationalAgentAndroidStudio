12f3b395ec95512224d3f4ace90277599be4f7ca
elisavetg <125077203+elisavetg@users.noreply.github.com>
2023-12-05 18:31:08 +0200

    Update SenderType.kt

--- app/src/main/java/com/example/cbink/SenderType.kt
+++ app/src/main/java/com/example/cbink/SenderType.kt
@@ -1,6 +1,13 @@
 package com.example.cbink
 
-enum class SenderType {
-    USER,
-    BOT
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+interface RasaService {
+    @POST("/webhooks/rest/webhook")
+    fun sendMessage(@Body messageRequest: MessageRequest): Call<List<MessageResponse>>
 }
+
+data class MessageRequest(val sender: String, val message: String)
+data class MessageResponse(val text: String)
